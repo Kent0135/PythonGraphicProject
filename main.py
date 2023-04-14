16bb593786acd9af436a18c8dfb4d4724d653ff1
Kent0135 <ktakeda@cocc.edu>
2023-04-14 13:54:58 -0700

    hi

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plot
 # set up your lists
-numlist = [8, 6, 5, 3]
+numlist = [18.3, 12.3, 6.3, 3.3]
 namelist = ['freshmen', 'sophomores', 'juniors', 'seniors']
 colorlist = ['red', 'green', 'pink', 'yellow' ]
-explodelist = [0.1, 0.0, 0.0, 0.0]
+explodelist = [0.0, 0.0, 0.0, 0.0]
 # make the pie chart
 plot.pie(numlist, labels=namelist, autopct='%.2f%%', colors=colorlist,
     	explode = explodelist, startangle = 90)
